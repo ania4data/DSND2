4a6be5b70fd4dac8ee11980bc4d186bca86753f8
Anoosheh Niavarani <niavarani_a@hotmail.com>
2018-10-15 19:01:55 -0700

    fix whitespaces

--- Software_engineering_part2/compute_launch.py
+++ Software_engineering_part2/compute_launch.py
@@ -4,7 +4,7 @@ def days_until_launch(current_day, launch_day):
     current_day (int) - current day in integer
     launch_day (int) - launch day in integer
     """
-    if(launch_day - current_day)<0 or launch_day<0 or current_day<0:
+    if(launch_day - current_day) < 0 or launch_day < 0 or current_day < 0:
         return 0
     else:
         return launch_day - current_day
