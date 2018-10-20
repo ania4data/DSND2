90a1fe37fb6a2ad8f872789e6191f500cc2c04c2
Anoosheh Niavarani <niavarani_a@hotmail.com>
2018-10-19 19:24:20 -0700

    add tqdm
    fixed issues with zoom and crop, added tqdm

--- Object_oriented_programming/wikimedia_package/wikimedia_test/getgloballinks.py
+++ Object_oriented_programming/wikimedia_package/wikimedia_test/getgloballinks.py
@@ -6,9 +6,10 @@ from PIL import Image
 import requests
 from io import BytesIO
 import numpy as np
+from tqdm import tqdm
 
 
-html_page = urllib.request.urlopen('https://commons.wikimedia.org/wiki/Commons:Quality_images/Subject/Sunsets') 
+html_page = urllib.request.urlopen('https://commons.wikimedia.org/wiki/Commons:Quality_images/Technical/Exposure') 
 soup = BeautifulSoup(html_page, 'lxml')
 images = []
 data_name = {}
@@ -53,22 +54,60 @@ with open('data_name.json', 'w') as outfile:
     json.dump(json_data, outfile)
 
 #print(name_list)
-print(address_list_original)
-print(len(address_list_original))
+print(address_list_thumb)
+print(len(address_list_thumb))
 
-random_link = np.random.choice(address_list_thumb,100)
+#random_link = np.random.choice(address_list_thumb,100)
+random_index = list(np.random.choice(range(len(address_list_thumb)), 100))
 
-new_img = Image.new('RGB', (500,500))
+#random_link = address_list_thumb[random_index[:]]
 
-for k, image_link in enumerate(random_link):
 
+new_img = Image.new('RGB', (600,600))
+
+
+for k in tqdm(range(100)):
+
+	#print('here')
+	image_link = address_list_thumb[random_index[k]]
+	#print(image_link)
+	#print('here')
 	response = requests.get(image_link)
 	img = Image.open(BytesIO(response.content))
-	img.thumbnail((50,50))
+	#print(img.width,img.height)
+
+	ratio=img.width/img.height
+
+	if(ratio<1.0):
+		new_height=int(60/ratio)
+		img_resize=img.resize((60,new_height))
+	else:
+		new_width=int(60*ratio)
+		img_resize=img.resize((new_width,60))
+
+	#img_resize.save('{}.png'.format(k))
+	center_y=int(img_resize.width/2.)
+	center_x=int(img_resize.height/2.)
+
+
+	upper = center_y- 30   # PIL  real x,is not actually along width, width->y   height->x
+	left = center_x - 30
+	lower = center_y + 30
+	right = center_x + 30
+
+
+	img_resize_crop=img_resize.crop((upper,left,lower,right))
+
+
+	#img_resize_crop.thumbnail((60,60), Image.ANTIALIAS)
 	j = int(k/10)
 	i = int(k - (j*10))
-	print(k)
+	#print(k)
 
-	new_img.paste(img, (i*50,j*50))
+	new_img.paste(img_resize_crop, (i*60,j*60))
+
+	#k += 1
+	# if(k%10 == 0):
+	# 	print('{}/100'.format(k))
 
 new_img.save("hola.png")
